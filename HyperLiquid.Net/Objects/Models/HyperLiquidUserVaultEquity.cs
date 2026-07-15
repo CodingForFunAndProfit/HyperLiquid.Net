7dae7a7b7c46d5a701e82b5e68c3d8c3d3dd0a6b
floxn <coding.forfun@protonmail.com>
2026-07-15 22:12:04 +0200

    first changes

---
+++ HyperLiquid.Net/Objects/Models/HyperLiquidUserVaultEquity.cs
@@ -0,0 +1,28 @@
+﻿using CryptoExchange.Net.Converters.SystemTextJson;
+using System;
+using System.Text.Json.Serialization;
+
+namespace HyperLiquid.Net.Objects.Models;
+
+/// <summary>
+/// User Vault Equity
+/// </summary>
+[SerializationModel]
+public record HyperLiquidUserVaultEquity
+{
+    /// <summary>
+    /// ["<c>vaultAddress</c>"] Vault address
+    /// </summary>
+    [JsonPropertyName("vaultAddress")]
+    public string VaultAddress { get; set; } = string.Empty;
+    /// <summary>
+    /// ["<c>equity</c>"] Equity
+    /// </summary>
+    [JsonPropertyName("equity")]
+    public string Equity { get; set; } = string.Empty;
+    /// <summary>
+    /// ["<c>lockedUntilTimestamp</c>"] Locked until timestamp
+    /// </summary>
+    [JsonPropertyName("lockedUntilTimestamp")]
+    public DateTime LockedUntilTimestamp { get; set; }
+}
